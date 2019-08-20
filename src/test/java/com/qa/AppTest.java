9050bce2b807d99317bc7c783d9a10c49209c69e
Jacob <jacob.eddison@qa.com>
2019-08-20 14:16:40 +0100

    BlackJack Test

--- src/test/java/com/qa/AppTest.java
+++ src/test/java/com/qa/AppTest.java
@@ -1,6 +1,7 @@
 package com.qa;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
@@ -16,6 +17,14 @@ public class AppTest
         app = new App();
         String[] input = new String[]{"a"};
         app.main(input);
+    }
+    @Test
+    public void blackJackTest(){
+        app = new App();
+        assertEquals("Expected: ",21,app.blackJack(21,18));
+        assertEquals("Expected: ",20, app.blackJack(20,15));
+        assertEquals("Expected: ",4, app.blackJack(23,4));
+        assertEquals("Expected: ",0,app.blackJack(23,24));
     }
     /**
      * Rigorous Test :-)
