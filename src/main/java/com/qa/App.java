f638672ce75986be1e178db4c46c2047cecde4f6
Jacob <jacob.eddison@qa.com>
2019-08-20 14:17:08 +0100

    BlackJack App

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -7,9 +7,29 @@ package com.qa;
 public class App 
 {
 
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
         System.out.println(args[0]);
-        System.out.println( "Hello World!" );
+        System.out.println("Hello World");
+
     }
+
+    public int blackJack(int hand1, int hand2) {
+        if ((hand1 >21 | hand1 <=0) & (hand2 >21 | hand2 <=0)) {
+            return 0;
+        }
+        else if (hand1 > 21 | hand1 <= 0) {
+            return hand2;
+        }
+        else if (hand2 > 21 | hand2 <= 0) {
+            return hand1;
+        }
+        else if (hand1 > hand2) {
+            return hand1;
+        }
+        else {
+            return hand2;
+        }
+    }
+
 }
+
